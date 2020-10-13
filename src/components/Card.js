91f402b8cee7f8ee23aa4d415341557c7d2b8ea0
Alexander Glazyrin <glazyrin-ai@yandex.ru>
2020-10-13 16:11:29 +0300

    fixed bugs of stats

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,9 +5,12 @@ const Card = ({playerInfo, stats, photo}) => {
       <div className="card text-center" style={{width: "30%", marginBottom: "20px"}}>
         <div className="card-body">
           <h5 className="card-title">{playerInfo.fullName}</h5>
-          <img src={photo} alt=""/>
-          <p className="card-text">{playerInfo.primaryPosition.code !== 'G' ? `Goals - ${stats ? stats.stat.goals : 0}, Assists - ${stats ? stats.stat.assists : 0}` :
-            `GP - ${stats ? stats.stat.games : 0}, Save % - ${stats ? stats.stat.evenStrengthSavePercentage.toFixed(2) : 0}`}</p>
+          <img src={photo} alt="" style={{margin: '8px'}}/>
+          <p className="card-text">
+            {playerInfo.primaryPosition.code !== 'G' ?
+            `GP - ${stats ? stats.stat.games : 0}, Goals - ${stats ? stats.stat.goals : 0}, Assists - ${stats ? stats.stat.assists : 0}` :
+            `GP - ${stats ? stats.stat.games : 0}, Save % - ${stats ? (stats.stat.savePercentage && stats.stat.savePercentage.toFixed(2)) : 0}, GAA - ${stats ? (stats.stat.goalAgainstAverage && stats.stat.goalAgainstAverage.toFixed(2)) : 0}`}
+          </p>
         </div>
       </div>
   );
